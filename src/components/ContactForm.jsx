ff8ccd9c4579d9102813dbe20f0ccc52f75a7ce1
JudeTheDuud <praiseosai@gmail.com>
2025-05-21 17:07:37 +0100

    Add Quote functionality with EmailJs, Toast for successful/failed form submission

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,9 +3,10 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 import styles from "../styles/contactForm.module.css";
 import { motion } from "framer-motion";
+import emailjs from "emailjs-com";
+import { ToastContainer, toast } from "react-toastify";
 
 const ContactForm = () => {
-  
   const fadeInAnimatonVariants = {
     initial: {
       opacity: 0,
@@ -57,141 +58,196 @@ const ContactForm = () => {
       projectDescription: "",
     },
     validationSchema,
-    onSubmit: (values) => {
-      console.log("Form submitted with values:", values);
-      alert("Form submitted successfully!");
+    onSubmit: (values, { resetForm }) => {
+      emailjs
+        .send(
+          "Bizinez4eva!", // Replace with your EmailJS service ID
+          "template_fqt7ubg", // Replace with your EmailJS template ID
+          {
+            user_name: values.name,
+            user_email: values.email,
+            user_phone: values.phone,
+            company_name: values.companyName,
+            company_size: values.companySize,
+            industry: values.industry,
+            budget: values.budget,
+            target_audience: values.targetAudience,
+            specific_services: values.specificServices,
+            project_timeline: values.projectTimeline,
+            project_description: values.projectDescription,
+          },
+          "2nEkYXBGOWT32Qznq" // Replace with your EmailJS user/public key
+        )
+        .then(
+          () => {
+            toast.success("Email sent successfully!");
+            resetForm();
+          },
+          (err) => {
+            console.error("FAILED...", err);
+            toast.error("Failed to send email. Please try again later.");
+          }
+        );
     },
   });
 
   return (
-    <div className={styles.container}>
-      <div className={styles.wrapper}>
-        <div className={styles.sectionA}>
-          <h3>Request a Quote</h3>
-          <p>Entrust with high professionalism</p>
-        </div>
-        <motion.div
-        variants={fadeInAnimatonVariants} 
-        initial = 'initial'
-        whileInView= 'animate'
-        className={styles.formWrap}>
-          <form onSubmit={formik.handleSubmit} className={styles.form}>
-            <div className={styles.formGroup}>
-              <label htmlFor="name">Name</label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                placeholder="John Doe"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.name}
-              />
-              <div
-                className={`${styles.error} ${
-                  formik.touched.name && formik.errors.name ? `${styles.visible}` : ``
-                }`}
-              >
-                {formik.touched.name && formik.errors.name}
-              </div>
-            </div>
-
-            <div className={styles.formGroup}>
-              <label htmlFor="email">Email</label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="email@company.com"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.email}
-              />
-              <div
-                className={`${styles.error} ${
-                  formik.touched.email && formik.errors.email ? `${styles.visible}` : ``
-                }`}
-              >
-                {formik.touched.email && formik.errors.email}
-              </div>
-            </div>
-
-            <div className={styles.formGroup}>
-              <label htmlFor="phone">Phone Number</label>
-              <input
-                type="text"
-                id="phone"
-                name="phone"
-                placeholder="09037586907"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.phone}
-              />
-             <div
-                className={`${styles.error} ${
-                  formik.touched.phone && formik.errors.phone ? `${styles.visible}` : ``
-                }`}
-              >
-                {formik.touched.phone && formik.errors.phone}
-              </div>
-            </div>
-
-            {/** Other fields **/}
-            {[
-              { id: "companyName", label: "Company Name" },
-              { id: "companySize", label: "Company Size" },
-              { id: "industry", label: "Industry" },
-              { id: "budget", label: "Budget" },
-              { id: "targetAudience", label: "Target Audience" },
-              { id: "specificServices", label: "Specific Services Required" },
-              { id: "projectTimeline", label: "Project Timeline" },
-            ].map(({ id, label }) => (
-              <div className={styles.formGroup} key={id}>
-                <label htmlFor={id}>{label}</label>
+    <>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        
+      />
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <div className={styles.sectionA}>
+            <h3>Request a Quote</h3>
+            <p>Entrust with high professionalism</p>
+          </div>
+          <motion.div
+            variants={fadeInAnimatonVariants}
+            initial="initial"
+            whileInView="animate"
+            className={styles.formWrap}
+          >
+            <form onSubmit={formik.handleSubmit} className={styles.form}>
+              <div className={styles.formGroup}>
+                <label htmlFor="name">Name</label>
                 <input
                   type="text"
-                  id={id}
-                  name={id}
-                  placeholder=""
+                  id="name"
+                  name="name"
+                  placeholder="John Doe"
                   onChange={formik.handleChange}
                   onBlur={formik.handleBlur}
-                  value={formik.values[id]}
+                  value={formik.values.name}
                 />
                 <div
-                className={`${styles.error} ${
-                  formik.touched[id] && formik.errors[id] ? `${styles.visible}` : ``
-                }`}
-              >
-                {formik.touched[id] && formik.errors[id]}
+                  className={`${styles.error} ${
+                    formik.touched.name && formik.errors.name
+                      ? `${styles.visible}`
+                      : ``
+                  }`}
+                >
+                  {formik.touched.name && formik.errors.name}
+                </div>
               </div>
-              </div>
-            ))}
 
-            <div className={`${styles.formGroup} ${styles.fullWidth}`}>
-              <label htmlFor="projectDescription">Project Description</label>
-              <textarea
-                id="projectDescription"
-                name="projectDescription"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.projectDescription}
-              />
-              <div
-                className={`${styles.error} ${
-                  formik.touched.projectDescription && formik.errors.projectDescription ? `${styles.visible}` : ``
-                }`}
-              >
-                {formik.touched.projectDescription && formik.errors.projectDescription}
+              <div className={styles.formGroup}>
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="email@company.com"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
+                />
+                <div
+                  className={`${styles.error} ${
+                    formik.touched.email && formik.errors.email
+                      ? `${styles.visible}`
+                      : ``
+                  }`}
+                >
+                  {formik.touched.email && formik.errors.email}
+                </div>
               </div>
-            </div>
 
-            <button disabled={formik.isSubmitting} type="submit">
-              Request a Quote
-            </button>
-          </form>
-        </motion.div>
+              <div className={styles.formGroup}>
+                <label htmlFor="phone">Phone Number</label>
+                <input
+                  type="text"
+                  id="phone"
+                  name="phone"
+                  placeholder="09037586907"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.phone}
+                />
+                <div
+                  className={`${styles.error} ${
+                    formik.touched.phone && formik.errors.phone
+                      ? `${styles.visible}`
+                      : ``
+                  }`}
+                >
+                  {formik.touched.phone && formik.errors.phone}
+                </div>
+              </div>
+
+              {/** Other fields **/}
+              {[
+                { id: "companyName", label: "Company Name" },
+                { id: "companySize", label: "Company Size" },
+                { id: "industry", label: "Industry" },
+                { id: "budget", label: "Budget" },
+                { id: "targetAudience", label: "Target Audience" },
+                { id: "specificServices", label: "Specific Services Required" },
+                { id: "projectTimeline", label: "Project Timeline" },
+              ].map(({ id, label }) => (
+                <div className={styles.formGroup} key={id}>
+                  <label htmlFor={id}>{label}</label>
+                  <input
+                    type="text"
+                    id={id}
+                    name={id}
+                    placeholder=""
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values[id]}
+                  />
+                  <div
+                    className={`${styles.error} ${
+                      formik.touched[id] && formik.errors[id]
+                        ? `${styles.visible}`
+                        : ``
+                    }`}
+                  >
+                    {formik.touched[id] && formik.errors[id]}
+                  </div>
+                </div>
+              ))}
+
+              <div className={`${styles.formGroup} ${styles.fullWidth}`}>
+                <label htmlFor="projectDescription">Project Description</label>
+                <textarea
+                  id="projectDescription"
+                  name="projectDescription"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.projectDescription}
+                />
+                <div
+                  className={`${styles.error} ${
+                    formik.touched.projectDescription &&
+                    formik.errors.projectDescription
+                      ? `${styles.visible}`
+                      : ``
+                  }`}
+                >
+                  {formik.touched.projectDescription &&
+                    formik.errors.projectDescription}
+                </div>
+              </div>
+
+              <button disabled={formik.isSubmitting} type="submit">
+                Request a Quote
+              </button>
+            </form>
+          </motion.div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
